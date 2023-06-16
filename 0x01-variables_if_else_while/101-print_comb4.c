851d81680dcc4dee5442e4b725d1991a93b7398b
abdallaabdalhalem9 <abdallaabdalhalem9@gmail.com>
2023-06-16 14:46:13 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int x, y, z;
+
+	for (x = 48; x <= 57; x++)
+	{
+		for (y = x + 1; y <= 57; y++)
+		{
+			for (z = y + 1; z <= 57; z++)
+			{
+				putchar(x);
+				putchar(y);
+				putchar(z);
+				if (x == 55 && y == 56 && z == 57)
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
