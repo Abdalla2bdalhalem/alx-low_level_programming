e619fa22cb1743febdb19060a340a0f0a3591ed9
abdallaabdalhalem9 <abdallaabdalhalem9@gmail.com>
2023-06-16 14:11:32 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int y = 0;
+
+	while (y != 10)
+	{
+		printf("%d", y);
+		y++;
+	}
+	putchar('\n');
+	return (0);
+}
