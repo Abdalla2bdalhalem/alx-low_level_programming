fe02b3d04050e073ef018ecefa1dbd6f1aa73ff4
abdallaabdalhalem9 <abdallaabdalhalem9@gmail.com>
2023-06-16 14:33:15 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+
+	int x = 49;
+	int y = 48;
+
+	while (y != 58)
+	{
+		while (x != 58)
+		{
+			if (x != y && y < i)
+			{
+				putchar(y);
+				putchar(x);
+				if (x == 57 && y == 56)
+				{
+					break;
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			x++;
+		}
+		x = 49;
+		y++;
+	}
+	putchar('\n');
+	return (0);
+
+}
