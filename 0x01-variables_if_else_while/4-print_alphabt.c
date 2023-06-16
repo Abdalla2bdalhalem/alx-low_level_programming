f535ba9fcabebba997a932242348f96c7b53e925
abdallaabdalhalem9 <abdallaabdalhalem9@gmail.com>
2023-06-16 14:06:08 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char y;
+
+	for (y = 'a'; y <= 'z'; x++)
+		if (y == 'e' || y == 'q')
+			continue;
+		else
+			putchar(y);
+	putchar('\n');
+	return (0);
+}
