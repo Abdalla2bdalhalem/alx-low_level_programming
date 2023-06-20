2e98f0fccc1ebb2a4573f59de2ba981a7b827004
abdallaabdalhalem9 <abdallaabdalhalem9@gmail.com>
2023-06-20 06:02:38 -0700

    hi

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -35,7 +35,7 @@ unsigned long f1 = 1, f2 = 2, sum, max = 100000000, f1o = 0, f2o = 0, smo = 0;
 	for (count = 1; count <= 98; count++)
 	{
 		if (f1o > 0)
-			printf("%u", f1o);
+			printf("%i", f1o);
 		initial0s = numLength(mx) - 1 - numLength(f1);
 		while (f1o > 0 && initial0s > 0)
 		{
