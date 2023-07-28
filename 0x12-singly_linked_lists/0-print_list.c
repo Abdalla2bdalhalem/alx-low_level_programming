c1e468f185e8eacd4aead5cd379a76326e0bce29
Abdalla <abdallaabdalhalem9@gmail.com>
2023-07-28 06:26:13 -0700

    All

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,7 +12,7 @@
  */
 size_t print_list(const list_t *h)
 {
-	int x = 0;
+	int i = 0;
 
 	while (h)
 	{
@@ -20,7 +20,7 @@ size_t print_list(const list_t *h)
 			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
-		x++;
+		i++;
 		h = h->next;
 	}
 	return (i);
